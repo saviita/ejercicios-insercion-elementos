7ebe5e18f5ddace6de72daae21cb892684481179
saviita <taatt2000@gmail.com>
2024-07-04 10:06:19 +0200

    04/07

--- src/js/index.js
+++ src/js/index.js
@@ -202,3 +202,107 @@ const checkContent = event => {
 listInputElement.addEventListener('input', checkContent)
 
 //EJ8 Con este HTML consigue que al introducir un número POSITIVO y MAYOR de 0 se genere la tabla de multiplicar de ese número del 0 al 10 como elementos de la lista. En el caso de que el número no sea correcto o no haya número, el botón estará desactivado.
+const inputMultiplayElement = document.getElementById('input-multiplay')
+const btnMultiplayElement = document.getElementById('btn-multiplay')
+const listMultiplayElement = document.getElementById('list-multiplay')
+
+const multiplayNumber = event => {
+    const fragmentMultiplay = document.createDocumentFragment()
+    const number = inputMultiplayElement.value
+    console.log('hello')
+    for(let i = 0; i < 11; i++){
+        const result = number * i
+        const resultMultiplay = document.createElement('li')
+        resultMultiplay.textContent = `${number} x ${i} = ${result}`
+        fragmentMultiplay.append(resultMultiplay)
+    }
+
+    listMultiplayElement.textContent = ''
+    listMultiplayElement.append(fragmentMultiplay)
+}
+btnMultiplayElement.addEventListener('click', multiplayNumber)
+
+//EJ9 Con este objeto debes crear tarjetas de usuario que muestren todos los datos, el diseño es libre, lo importante es que muestren toda la información del usuario y la imagen de perfil. Crea una función que genere todas las tarjetas de usuario y las inserte en el DOM
+const USERS = [
+    {
+      name: 'Josep Flores',
+      age: 77,
+      username: 'Josep85',
+      email: 'Josep_Flores@hotmail.com',
+      profileImage: 'https://randomuser.me/api/portraits/women/24.jpg'
+    },
+    {
+      name: 'Ricardo Rosas',
+      age: 43,
+      username: 'Ricardo_Rosas',
+      email: 'Ricardo_Rosas22@yahoo.com',
+      profileImage: 'https://randomuser.me/api/portraits/men/57.jpg'
+    },
+    {
+      name: 'Iván Tamayo',
+      age: 40,
+      username: 'tamayo87',
+      email: 'Ivan_Tamayo61@yahoo.com',
+      profileImage: 'https://randomuser.me/api/portraits/men/9.jpg',
+      job: 'Lead Communications Designer'
+    },
+    {
+      name: 'Maica Villanueva',
+      age: 64,
+      username: 'Maica.Villanueva18',
+      email: 'Maica.Villanueva1@yahoo.com',
+      profileImage: 'https://randomuser.me/api/portraits/women/4.jpg'
+    },
+    {
+      name: 'Pedro Estrada',
+      age: 77,
+      username: 'Pedro29',
+      email: 'Pedro_Estrada22@hotmail.com',
+      profileImage: 'https://randomuser.me/api/portraits/men/2.jpg',
+      job: 'Central Directives Liaison'
+    },
+    {
+      name: 'Jorge Cedillo',
+      age: 33,
+      username: 'Jorge_Cedillo',
+      email: 'Jorge.Cedillo2@yahoo.com',
+      profileImage: 'https://randomuser.me/api/portraits/men/88.jpg'
+    }
+  ];
+  const cardsElement = document.getElementById('cards')
+
+  const createCard = () => {
+      const fragmentUser = document.createDocumentFragment()
+      
+      for(const field of USERS){
+        const newName = document.createElement('span')
+        newName.textContent = field.name
+
+        const newAge = document.createElement('span')
+        newAge.textContent = field.age
+
+        const newUsername = document.createElement('span')
+        newUsername.textContent = field.username
+
+        const newEmail = document.createElement('span')
+        newEmail.textContent = field.email
+
+        const newProfileImg = document.createElement('span')
+        newProfileImg.textContent = field.profileImage
+
+        const newCard = document.createElement('div')
+        if(field.job){
+            const newJob = document.createElement('span')
+            newJob.textContent = field.job
+            newCard.append(newName, newAge, newUsername, newEmail, newProfileImg, newJob)
+        } else {
+            newCard.append(newName, newAge, newUsername, newEmail, newProfileImg)
+        }
+
+
+        fragmentUser.append(newCard)
+    }
+    // document.body.append(fragmentUser)
+    cardsElement.append(fragmentUser)
+  }
+  createCard()
